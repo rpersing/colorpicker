f773cd5a8058e564996a9fb2004fe9dad971d994
rpersing <rpersing@mail.umw.edu>
2022-05-27 14:39:40 -0400

    updated routes and html forms

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import uuid
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def colorpicker():
+def parameters():
+
     if request.method == "POST":
+        client_name = request.form["clientname"]
+        num_colors = int(request.form["numcolors"])
+
+        # print(type(num_colors))
+
+        return render_template("colorpicker.html", num_colors=num_colors)
+
+    return render_template("parameters.html")
+
+@app.route("/colorpicker", methods=["GET", "POST"])
+def colorpicker():
+    
+    if request.method == "POST":
+
         color_value = request.form["colorpicker"]
         print(color_value)
+        id_ = uuid.uuid1()
+
     return render_template("colorpicker.html")
