85bb973e40d499835db31f47e435f491962e4bda
rpersing <rpersing@mail.umw.edu>
2022-05-27 13:36:57 -0400

    changed color picker to pure html

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def colorpicker():
+    if request.method == "POST":
+        color_value = request.form["colorpicker"]
+        print(color_value)
     return render_template("colorpicker.html")
