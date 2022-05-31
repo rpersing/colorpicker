9ec3786fe35a06fbc8664e48bf6f9f6071406773
rpersing <rpersing@mail.umw.edu>
2022-05-31 18:08:29 -0400

    dynamic color picking

--- app.py
+++ app.py
@@ -5,24 +5,21 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def parameters():
-
-    if request.method == "POST":
-        client_name = request.form["clientname"]
-        num_colors = int(request.form["numcolors"])
-
-        # print(type(num_colors))
-
-        return render_template("colorpicker.html", num_colors=num_colors)
-
     return render_template("parameters.html")
 
 @app.route("/colorpicker", methods=["GET", "POST"])
 def colorpicker():
-    
-    if request.method == "POST":
 
-        color_value = request.form["colorpicker"]
-        print(color_value)
-        id_ = uuid.uuid1()
+    if request.method == "POST":
+        client_name = request.form["clientname"]
+        num_colors = request.form["numcolors"]
+        return render_template("colorpicker.html", client_name=client_name, num_colors=int(num_colors))
 
     return render_template("colorpicker.html")
+
+@app.route("/css-file", methods=["GET", "POST"])
+def css_file():
+    if request.method == "POST":
+        print(request.form)
+        
+    return "File download"
